586b26a3af99433855e618de97464fc6b432d2ed
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-12-15 17:42:10 -0300

    Texto y Fuente
    Cargar Fuentes
    Dibujar Texto

--- main.lua
+++ main.lua
@@ -8,14 +8,23 @@ WINDOW_HEIGHT = 720
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+    
+    largeFont = love.graphics.newFont('font.ttf', 32)
+    smallFont = love.graphics.newFont('font.ttf', 8)
 
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    player1Score = 0
+    player2Score = 0
+
+    push:setupScreen(
+        VIRTUAL_WIDTH,
+        VIRTUAL_HEIGHT,
+        WINDOW_WIDTH, 
+        WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
-
 
 function love.keypressed(key)
     if key == 'escape' then
@@ -23,9 +32,19 @@ function love.keypressed(key)
     end
 end
 
-
 function love.draw()
     push:start()
-    love.graphics.printf(" Hola, Pong! ", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.clear(40/255, 45/255, 52/255,1)
+    -- texto
+    love.graphics.setFont(largeFont)
+    love.graphics.printf(" Hola, Pong! ", 0, 20, VIRTUAL_WIDTH, 'center')
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 + 80)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 + 80)
+    -- paleta 1
+    love.graphics.rectangle('fill', 10, 10, 5,20)
+    -- paleta 2
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5,20)
+    -- ball
+        love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2-2, 4,4)
     push:finish()
 end
