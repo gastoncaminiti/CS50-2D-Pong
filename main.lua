f08547ba8ecd0c33c7f4f26f49b1e4b6699373fe
Gaston Caminiti <gastoncaminiti@outlook.com>
2025-12-22 14:48:16 -0300

    Mover Paleta 1 y Paleta 2

--- main.lua
+++ main.lua
@@ -6,7 +6,10 @@ VIRTUAL_HEIGHT = 243
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+PADDLE_SPEED = 200
+
 function love.load()
+
     love.graphics.setDefaultFilter('nearest', 'nearest')
     
     largeFont = love.graphics.newFont('font.ttf', 32)
@@ -14,6 +17,9 @@ function love.load()
 
     player1Score = 0
     player2Score = 0
+
+    player1Y = 10
+    player2Y = VIRTUAL_HEIGHT - 30
 
     push:setupScreen(
         VIRTUAL_WIDTH,
@@ -32,6 +38,22 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+   
+end
+
 function love.draw()
     push:start()
     love.graphics.clear(40/255, 45/255, 52/255,1)
@@ -41,9 +63,9 @@ function love.draw()
     love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 + 80)
     love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 + 80)
     -- paleta 1
-    love.graphics.rectangle('fill', 10, 10, 5,20)
+    love.graphics.rectangle('fill', 10, player1Y, 5,20)
     -- paleta 2
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5,20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 5,20)
     -- ball
         love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2-2, 4,4)
     push:finish()
